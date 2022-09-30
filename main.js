708d7d059ccc61944315398ff49d9d1ea36d08cf
jacobSalazku <Cooljss10@gmail.com>
2022-09-30 13:26:42 +0200

    minify and uglify

---
+++ main.js
@@ -0,0 +1,2 @@
+let message=document.getElementById("message");function validateForm(){let e=document.getElementById("name-input");return""==
+e.value?(alert("Geef een naam in aub"),!1):(message.innerHTML="hey "+e.value,!1)}
