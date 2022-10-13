d5f353ae8cd6763bec60170ce159c1aecc366969
jacobSalazku <Cooljss10@gmail.com>
2022-10-13 13:47:40 +0200

    aanpassing app.js en main.js console log

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
 let message=document.getElementById("message");function validateForm(){let e=document.getElementById("name-input");return""==
 e.value?(alert("Geef een naam in aub"),!1):(message.innerHTML="hey "+e.value,!1)}
+
+console.log("Hello repo")
